1455cee017b01026e05d950c6ea2847e4f8f5ad9
Mrigank923 <mrigankraj23@gmail.com>
2024-11-04 14:56:43 +0530

    resolving merge issue

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from .views import *
 
 
@@ -14,6 +14,7 @@ urlpatterns = [
     path('registrationOTP/', RegistrationOTPView.as_view(), name='register_otp'),
     path('register/', RegisterWithOTPView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('forget-password/', ForgetPassword.as_view(), name='forget-password'),
     path('verify-otp/', OTPVerificationView.as_view(), name='verify-otp'),
