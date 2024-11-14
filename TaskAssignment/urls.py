9583e305d1076da7b2503478eba52499c525f4ef
Mrigank923 <mrigankraj23@gmail.com>
2024-11-15 02:35:28 +0530

    created api for update, delete, read task for admin, manager, recep and also task status update for staff

--- TaskAssignment/urls.py
+++ TaskAssignment/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import Taskassignment
+from .views import Taskassignment,TaskDeleteView,TaskUpdateView,AllTaskListView,StaffTaskListView,TaskStatusUpdateView
 
 urlpatterns = [
     path('tasks/', Taskassignment.as_view(), name='task-create'),
+    path('tasks/delete/<int:pk>/', TaskDeleteView.as_view()),
+    path('tasks/update/<int:pk>/', TaskUpdateView.as_view()),
+    path('tasks/all/', AllTaskListView.as_view()),
+    path('tasks/staff/',StaffTaskListView.as_view()),
+    path('tasks/status/<int:pk>/', TaskStatusUpdateView.as_view(),)
+
+    
+    
+   
 ]
