79f91c32983f8c7e86f65708daeec5957e7c4a95
Ritik-Tayal <ritik23153049@akgec.ac.in>
2024-11-17 16:28:27 +0530

    added department in attendance response

--- edit_profiles/serializers.py
+++ edit_profiles/serializers.py
@@ -13,10 +13,8 @@ class StaffListSerializer(serializers.ModelSerializer):
         fields = ['id', 'user_name','email', 'role','department']
 
     def get_department(self, obj):
-        # Check if the user's role is Staff
         if obj.role == 'Staff':
             try:
-                # Return the related Staff model's department
                 return obj.staff_profile.department
             except Staff.DoesNotExist:
                 return None
@@ -26,7 +24,7 @@ class UserSerializer(serializers.ModelSerializer):
     user_name = serializers.CharField(required=True)
     email = serializers.EmailField(required=True)
     role = serializers.ChoiceField(choices=User.ROLE_CHOICES, required=True)
-    department = serializers.CharField(required=False, allow_blank=True)  # Relevant for staff only
+    department = serializers.CharField(required=False, allow_blank=True)
 
     class Meta:
         model = User
