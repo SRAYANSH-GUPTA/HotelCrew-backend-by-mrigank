e31be8cc2a070b74e9a0d07fbff181a691745d56
Ritik-Tayal <ritik23153049@akgec.ac.in>
2024-11-26 13:31:22 +0530

    added customer status

--- hoteldetails/models.py
+++ hoteldetails/models.py
@@ -63,6 +63,7 @@ class Customer(models.Model):
     room_no = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     checked_out = models.BooleanField(default=False) 
+    status=models.CharField(max_length=10,default="Regular")
     # room_released = models.BooleanField(default=False)  # Track if the room has been released
 
     # def save(self, *args, **kwargs):
