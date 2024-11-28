b7fdad4c3305247b7d78e4b75fb9c6082cbd2c83
Mrigank923 <mrigankraj23@gmail.com>
2024-11-28 18:03:07 +0530

    made changes in hotelupdate serializer

--- edit_profiles/serializers.py
+++ edit_profiles/serializers.py
@@ -49,13 +49,46 @@ class RoomTypeSerializer(serializers.ModelSerializer):
         fields = ['room_type', 'count', 'price']
         
 class HotelUpdateSerializer(serializers.ModelSerializer):
-    room_types = RoomTypeSerializer(many=True, required=False)
+    room_types = RoomTypeSerializer(many=True, required=False)  # Nested serializer for room types
 
-    
     class Meta:
-        model = HotelDetails
-        exclude = ['user']
-        
+        model = HotelDetails  
+        fields = [
+            'hotel_name',
+            'legal_business_name',
+            'year_established',
+            'license_registration_numbers',
+            'complete_address',
+            'main_phone_number',
+            'emergency_phone_number',
+            'email_address',
+            'total_number_of_rooms',
+            'number_of_floors',
+            'valet_parking_available',
+            'valet_parking_capacity',
+            'check_in_time',
+            'check_out_time',
+            'payment_methods',
+            'room_price',
+            'number_of_departments',
+            'department_names',
+            'room_types',
+        ]
+
+    def update(self, instance, validated_data):
+
+        room_types_data = validated_data.pop('room_types', None)
+        if room_types_data:
+            instance.room_types.all().delete()
+            for room_type_data in room_types_data:
+                RoomType.objects.create(hotel=instance, **room_type_data)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
 class ProfileViewSerializer(serializers.ModelSerializer):
     department = serializers.SerializerMethodField()
     shift= serializers.SerializerMethodField()    
