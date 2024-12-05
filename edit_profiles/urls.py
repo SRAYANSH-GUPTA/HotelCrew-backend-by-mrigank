425b52460687fd6d83bbe1b5b93ea7af67ec46bc
Mrigank923 <mrigankraj23@gmail.com>
2024-12-05 23:14:34 +0530

    added pagination in staff get list

--- edit_profiles/urls.py
+++ edit_profiles/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('schedule_list/',ScheduleListView.as_view(),name='schedule-list'),
     path('schedule_change/<int:user_id>/',ChangeShiftView.as_view(),name='schedule-change'),
     path('department_list/',TotalDepartmentsView.as_view(),name='department-list'),
+    path('pagination/staff_list/',StaffPaginationListView.as_view(),name='staff-list-pagination'),
 ]
